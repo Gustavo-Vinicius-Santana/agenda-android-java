c8ff2dc94723f126b31db44d1950c239f95df8fa
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-05-17 11:45:17 -0300

    hello world na activity

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -11,6 +12,11 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         Toast.makeText(this, "Hello world!", Toast.LENGTH_LONG).show();
+
+        TextView mensage = new TextView(this);
+        mensage.setText("hello world!");
+        setContentView(mensage);
     }
 }
