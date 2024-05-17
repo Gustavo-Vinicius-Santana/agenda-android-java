7f65f77dc9a53f20b7ce2aa4d1f552f7cc47c8a4
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-05-17 13:24:07 -0300

    text view para cada aluno e definição dos valores pelo main

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -7,6 +7,10 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends Activity {
 
     @Override
@@ -16,5 +20,17 @@ public class MainActivity extends Activity {
         Toast.makeText(this, "Hello world!", Toast.LENGTH_LONG).show();
 
         setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "josé", "maria"));
+
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView2);
+        TextView terceiroAluno = findViewById(R.id.textView3);
+        TextView quartoAluno = findViewById(R.id.textView4);
+
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
+        quartoAluno.setText(alunos.get(3));
     }
 }
