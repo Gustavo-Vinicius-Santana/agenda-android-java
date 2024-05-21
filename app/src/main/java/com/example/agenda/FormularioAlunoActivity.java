41902c4756368377e62de6059a649e6893ca3521
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-05-21 20:23:00 -0300

    pegando dados do layout e imprimindo no toast

--- app/src/main/java/com/example/agenda/FormularioAlunoActivity.java
+++ app/src/main/java/com/example/agenda/FormularioAlunoActivity.java
@@ -1,12 +1,18 @@
 package com.example.agenda;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.agenda.models.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -15,10 +21,30 @@ public class FormularioAlunoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_formulario_aluno);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_btn_enviar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = campoNome.getText().toString();
+                String email = campoEmail.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+
+                Aluno alunoCriado = new Aluno(nome, email, telefone);
+                Toast.makeText(FormularioAlunoActivity.this,
+                        alunoCriado.getNome() + " - " + alunoCriado.getEmail() + " - " + alunoCriado.getTelefone(),
+                        Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
---
+++ app/src/main/java/com/example/agenda/models/Aluno.java
@@ -0,0 +1,25 @@
+package com.example.agenda.models;
+
+public class Aluno {
+    private final String nome;
+    private final String email;
+    private final String telefone;
+
+    public Aluno(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+}
