9f01d12760c3db82dd0fe55a7ae17782050ff755
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-05-22 11:19:47 -0300

    fluxo do inicial do aplicativo

--- app/src/main/java/com/example/agenda/FormularioAlunoActivity.java
+++ app/src/main/java/com/example/agenda/FormularioAlunoActivity.java
@@ -16,10 +16,12 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.agenda.dao.alunoDAO;
 import com.example.agenda.models.Aluno;
 
+import org.jetbrains.annotations.Nullable;
+
 public class FormularioAlunoActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_formulario_aluno);
--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.agenda;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -10,6 +13,7 @@ import androidx.annotation.Nullable;
 
 import com.example.agenda.dao.alunoDAO;
 import com.example.agenda.models.Aluno;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,13 +27,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main_lista_alunos);
         setTitle("lista de alunos");
 
-        alunoDAO dao = new alunoDAO();
-        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "josé", "maria"));
+        FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_main_abrir_formulario);
+        botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FormularioAlunoActivity.class));
+            }
+        });
 
+        alunoDAO dao = new alunoDAO();
         ListView listaAlunos = findViewById(R.id.activity_main_list_alun);
         listaAlunos.setAdapter(new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,
                 dao.todos()));
-
     }
 }
